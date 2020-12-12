7fa2a395e3143a28ad0720fc5567211ee49bc881
mingzhi <hsy202161@alibaba-inc.com>
2020-12-12 14:59:44 +0800

    leetcode 376题解-摆动序列

---
+++ Solution376.java
@@ -0,0 +1,43 @@
+/**
+ *
+ * 376. 摆动序列
+ *
+ * 如果连续数字之间的差严格地在正数和负数之间交替，则数字序列称为摆动序列。第一个差（如果存在的话）可能是正数或负数。少于两个元素的序列也是摆动序列。
+ *
+ * 例如， [1,7,4,9,2,5] 是一个摆动序列，因为差值 (6,-3,5,-7,3) 是正负交替出现的。相反, [1,4,7,2,5] 和 [1,7,4,5,5]
+ * 不是摆动序列，第一个序列是因为它的前两个差值都是正数，第二个序列是因为它的最后一个差值为零。
+ *
+ * 给定一个整数序列，返回作为摆动序列的最长子序列的长度。 通过从原始序列中删除一些（也可以不删除）元素来获得子序列，剩下的元素保持其原始顺序。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,7,4,9,2,5]
+ * 输出: 6
+ * 解释: 整个序列均为摆动序列
+ *
+ * @author mingzhi
+ * @date 2020-12-12
+ */
+public class Solution376 {
+
+    public int wiggleMaxLength(int[] nums) {
+        return greedyMethod(nums);
+    }
+
+    private int greedyMethod(int[] nums) {
+        int n = nums.length;
+        if (n < 2) {
+            return n;
+        }
+        int prevdiff = nums[1] - nums[0];
+        int ret = prevdiff != 0 ? 2 : 1;
+        for (int i = 2; i < n; i++) {
+            int diff = nums[i] - nums[i - 1];
+            if ((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0)) {
+                ret++;
+                prevdiff = diff;
+            }
+        }
+        return ret;
+    }
+}
